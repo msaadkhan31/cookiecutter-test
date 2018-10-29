35426c64133fac1c0a1d34c558ea8a3f427fb7ca
saad.khan <saad.khan@careem.com>
2018-10-29 13:36:08 +0500

    add file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,17 +2,9 @@ import logging
 import shutil
 import os
 
-DockerEnv = '{{ cookiecutter.dockerenv }}'
-
 
 def main():
     try:
-        if DockerEnv == '':
-            print('Not a docker environment')
-
-        else:
-            inplace_change("Makefile", "upload-eb-version",
-                           "create-push-docker-image")
         move_file("Makefile", "../Makefile")
     except ImportError:
         logging.debug("Unable to copy file")
@@ -20,19 +12,6 @@ def main():
 
 def move_file(source, desti):
     shutil.move(source, desti)
-
-
-def inplace_change(filename, old_string, new_string):
-    # Safely read the input filename using 'with'
-    with open(filename) as f:
-        s = f.read()
-        if old_string not in s:
-            return
-
-    # Safely write the changed content, if found in the file
-    with open(filename, 'w') as f:
-        s = s.replace(old_string, new_string)
-        f.write(s)
 
 
 if __name__ == '__main__':
